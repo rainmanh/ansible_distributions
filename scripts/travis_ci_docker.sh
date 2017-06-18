6f196f9e64c839ea550ef9bae834de7b491a2403
Gonzalo <g.fer.ordas@unicyber.co.uk>
2017-06-18 14:20:03 +0100

    README and Script fix

--- scripts/travis_ci_docker.sh
+++ scripts/travis_ci_docker.sh
@@ -21,12 +21,15 @@ done
 #Centos
 for centos_version in $centos_versions; do
 
-  eval export REPO_CENTOS_${!centos_version}=ansible_centos_${!centos_version}
+  eval export REPO_CENTOS_${!centos_version}=ansible_centos_${centos_version}
   eval export ANSIBLE_VERSION_CENTOS_${!centos_version}=$(docker run $DOCKER_USERNAME/$REPO_CENTOS_${!centos_version} /bin/bash -c 'ansible --version'|head -1 |awk '{print $2}')
   echo "ANSIBLE_VERSION_CENTOS_${centos_version}=$ANSIBLE_VERSION_CENTOS_${!centos_version}"
   eval export NEW_REPO_CENTOS_${!centos_version}="$REPO_CENTOS_${!centos_version}:ansible_$ANSIBLE_VERSION_CENTOS_${!centos_version}"
   echo "NEW_REPO_CENTOS_${centos_version}=$NEW_REPO_CENTOS_${!centos_version}"
   docker build -t $DOCKER_USERNAME/$NEW_REPO_CENTOS_${!centos_version} -f centos/${centos_version}/Dockerfile .
   docker push $DOCKER_USERNAME/$NEW_REPO_CENTOS_${!centos_version}
-  docker images
-  docker ps -a
+
+done
+
+docker images
+docker ps -a
